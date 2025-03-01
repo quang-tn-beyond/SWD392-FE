f67b06bf7b4a64c881b1b55894f8cf0afb30abc6
SWD <phongdtse180404@fpt.edu.vn>
2025-03-01 20:14:31 +0700

    search bar

--- Rookie Comics/src/Components/Header.jsx
+++ Rookie Comics/src/Components/Header.jsx
@@ -1,14 +1,21 @@
 // Header.jsx
 import { Link } from "react-router-dom";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import ProfileMenu from "./ProfileMenu";
 import { AuthContext } from "./AuthContext";
+import { comics } from "../data";
 
 export default function Header() {
   const { isLoggedIn, setIsLoggedIn } = useContext(AuthContext);
 
   // Xử lý logout không cần thiết ở đây vì đã có trong ProfileMenu, 
   // nhưng nếu cần bạn có thể gọi setIsLoggedIn(false) tại đây.
+  const [showSearch, setShowSearch] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const toggleSearch = () => {
+    setShowSearch(!showSearch);
+  };
 
   return (
     <header className="header">
@@ -80,7 +87,7 @@ export default function Header() {
               <Link to="/pricing" className="pricing-switch">
                 <span className="fas fa-dollar-sign"></span>
               </Link>
-              <Link to="/search" className="search-switch">
+              <Link to="/search" className="search-switch" onClick={toggleSearch}>
                 <span className="icon_search"></span>
               </Link>
               {isLoggedIn ? (
@@ -95,6 +102,31 @@ export default function Header() {
         </div>
         <div id="mobile-menu-wrap"></div>
       </div>
+
+
+      {/* searchBar */}
+      {showSearch && (
+        <div className="search-bar">
+          <input
+            type="text"
+            placeholder="Tìm kiếm truyện..."
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
+          <button className="search-icon">
+            <span className="icon_search"></span>
+          </button>
+          {/* Suggest Comic */}
+          <div className="search-results">
+            {searchQuery && comics.filter((comic) => comic.title.toLowerCase().includes(searchQuery.toLowerCase())).map((comic) => (
+              <div key={comic.id} className="search-item">
+                <img src={comic.imageUrl} alt={comic.title} />
+                <span>{comic.title}</span>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
     </header>
   );
 }
